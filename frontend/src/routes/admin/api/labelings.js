614baea4c6930f34a149ca212bf3d1a393e78ae8
mikolajkazmierczak <mikolaj@kazmierczak.pl>
2025-06-24 23:46:34 +0200

    WIP: automatic labelings mapping 2

--- frontend/src/routes/admin/api/labelings.js
+++ frontend/src/routes/admin/api/labelings.js
@@ -1,73 +1,69 @@
+import { get } from 'svelte/store';
 import { defaults } from '%/fields/labelings';
 import { companies, labelings } from '@/globals';
 
 // the map points to a company and a code
-const tempMapping = {
-  P1: 'REED:RT1',
-  P2: 'REED:RT1',
-  P3: 'MidOcean:P3',
+const mapping = {
+  P1: { company: 4, code: 'RT1' },
+  P2: { company: 4, code: 'RT1' },
+  P3: { company: 2, code: 'P3' },
   L2: {
-    price: {
-      x: 'REED:L1',
-      '>3': 'REED:L2',
-      '>5': 'REED:L3'
-    }
+    threshold: 'price',
+    values: [
+      { company: 4, code: 'L1', value: null, type: 'eq' },
+      { company: 4, code: 'L2', value: 3, type: 'gt' },
+      { company: 4, code: 'L3', value: 5, type: 'gt' }
+    ]
   },
   L3: {
-    area: {
-      x: 'REED:L3',
-      '>5': 'REED:L4',
-      '>10': 'REED:L5',
-      '>30': 'MidOcean:L3'
-    }
+    threshold: 'area', // area in mm^2
+    values: [
+      { company: 4, code: 'L3', value: null, type: 'gt' },
+      { company: 4, code: 'L4', value: 50, type: 'gt' },
+      { company: 4, code: 'L5', value: 100, type: 'gt' },
+      { company: 2, code: 'L3', value: 300, type: 'gt' }
+    ]
   }
 };
 
-function parseMapping(mapping) {
-  // parse the mapping to a more usable format
-  const parseCode = code => {
-    const parts = code.split(':');
-    const company = companies.find(c => c.name === parts[0]).id;
-    return { company, code: parts[1] };
+function newLabeling(company, id, index, label, height, width) {
+  // TODO: should the price calculation be done here? or after the item is created in +page.js?
+  // Create a new labeling object with the given parameters.
+  const global_margin = company === 2 ? false : true; // disabled in MidOcean
+  return {
+    index,
+    enabled: true, // enabled by default?
+    global_margin,
+    labeling: id,
+    labeling_field_x: width,
+    labeling_field_y: height,
+    labeling_place: label
   };
-  const parseArea = area => {
-    for (const key in area) {
-      const value = area[key];
-      const props =
-        key === 'x'
-          ? { type: 'default' }
-          : key.startsWith('>')
-            ? { type: 'gt', threshold: parseFloat(key.slice(1)) }
-            : key.startsWith('>=')
-              ? { type: 'gte', threshold: parseFloat(key.slice(2)) }
-              : key.startsWith('<')
-                ? { type: 'lt', threshold: parseFloat(key.slice(1)) }
-                : key.startsWith('<=') || key === 'default'
-                  ? { type: 'lte', threshold: parseFloat(key.slice(2)) }
-                  : {};
-      return props;
-    }
-  };
-  const result = {};
-  for (const key in mapping) {
-    const value = mapping[key];
-    if (typeof value === 'string') {
-      result[key] = parseCode(value);
-    } else {
-      result[key] = parseMapping(value);
-    }
-  }
-  return result;
 }
 
-export function getLabelings(data) {
-  // get labeling id based on the company and code
-  const output = [];
+export function createLabelings(data) {
+  // Create product labelings based on the data fetched from the API, and the mapping defined above.
+  // data: [{ techniques: ['CODE',...], label: 'top side', height: 0, width: 0, area: 0 }, ...]
+  const result = [];
+
   for (const labeling of data) {
     const { techniques, label, height, width, area } = labeling;
     for (const technique of techniques) {
-      const mapping = tempMap[technique];
-      // ...
+      if (!mapping[technique]) {
+        console.warn(`Labeling code ${technique} not found in mapping.`);
+        continue;
+      }
+
+      let index = 0;
+
+      const rule = mapping[technique];
+      if (rule.code) {
+        // Simple case: company and code are defined directly
+        // Only need to check if the labeling with such company and code exists.
+        const { company, code } = rule;
+        const l = get(labelings).find(l => l.company === company && l.code === code);
+        result.push(newLabeling(company, l?.id, index++, label, height, width));
+      }
     }
   }
 }
