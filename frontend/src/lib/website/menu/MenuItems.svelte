3328b79cb0032d6818d1d309a8819daa93adc6af
mikolajkazmierczak <mikolaj@kazmierczak.pl>
2025-06-29 02:16:39 +0200

    feat: a bit cleaner menu

--- frontend/src/lib/website/menu/MenuItems.svelte
+++ frontend/src/lib/website/menu/MenuItems.svelte
@@ -19,12 +19,11 @@
   const depth = items[0]._meta.depth;
   const lvl0 = depth == 0;
   const lvl1 = depth == 1;
-  const padding = (depth - 1) * 20 + 'px'; // don't indent lvl0
 </script>
 
 <!-- out:slide={{ duration: 150, delay: 150 }} -->
 <!-- out:fly={{ y: -15, duration: 150 }} -->
-<div class="menu" class:lvl0 class:lvl1 style:padding-left={padding} in:slide={{ duration: 300 }}>
+<div class="menu" class:lvl0 class:lvl1 in:slide={{ duration: 300 }}>
   {#each items as { id, href, name }, i (id)}
     {@const children = items.find(c => c.id === id)?.children}
     {@const childActive = deepFindItemByCategorySlug(children, $page.params.slug)}
@@ -33,9 +32,7 @@
       {name}
     </a>
     {#if children?.length && active}
-      <div class="submenu">
-        <svelte:self items={children} />
-      </div>
+      <svelte:self items={children} />
     {/if}
   {/each}
 </div>
@@ -45,22 +42,24 @@
     --bg-hover: var(--main-1);
     --bg-active: var(--main);
   }
-  .menu,
-  .submenu {
+  .menu {
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     gap: 0.75rem;
-  }
-  .submenu {
     border-left: 2px solid var(--bg-active);
+    padding-left: 1rem;
+  }
+  .lvl0,
+  .lvl1 {
+    padding-left: 0;
+    border-left: none;
   }
   .lvl0 {
     gap: 1.4rem;
   }
-  .lvl0 > .submenu {
+  .lvl1 {
     margin-top: -0.75rem;
-    border-left: none;
   }
 
   .item {
